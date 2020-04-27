e1d79403af157fd67a00d8f125657e282f73d7a2
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-04-28 01:31:59 +0200

    Added closed to observed attributes and set it in the connectedcallback if it is not detected. This way 'closed' is the default.

--- src/SketchInfo.js
+++ src/SketchInfo.js
@@ -16,9 +16,19 @@ const template = document.createElement('template');
   `;
 
 export class SketchInfo extends HTMLElement {
+  static get observedAttributes() {
+    return ['closed'];
+  }
+
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    if (!this.hasAttribute('closed')) {
+      this.setAttribute('closed', true)
+    }
   }
 }
